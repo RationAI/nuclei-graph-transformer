351b5acd0fc743169de6cc42bc6749e562538226
Karolína Rusnačková <525276@mail.muni.cz>
2026-01-04 08:51:51 +0000

    fix: var name mismatch

--- nuclei_graph/data/utils/sampler.py
+++ nuclei_graph/data/utils/sampler.py
@@ -14,21 +14,21 @@ def compute_slides_positivity(
     Args:
         metadata: DataFrame containing slide metadata with a "slide_id" (str) column.
         labels: DataFrame containing nuclei labels with "slide_id" (str), "id" (str)
-                and "annot_label" (int) columns.
-        label_mask: Optional DataFrame containing label indicators with "slide_id" (str),
-                    "id" (str) and "cam_thr_mask" (bool) columns.
+                and "label" (int) columns.
+        label_mask: Optional DataFrame containing label indicator with "slide_id" (str),
+                    "id" (str) and "refinement_mask" (bool) columns.
 
     Returns:
         A dictionary mapping each slide ID to its fraction of positive nuclei [0, 1].
     """
     if label_mask is not None:
         merged = labels.merge(label_mask, on=["slide_id", "id"], how="inner")
-        merged["pos_score"] = (merged["annot_label"] * merged["cam_thr_mask"]).astype(
+        merged["pos_score"] = (merged["label"] * merged["refinement_mask"]).astype(
             "uint8"
         )
         positivity_series = merged.groupby("slide_id")["pos_score"].mean()
     else:
-        positivity_series = labels.groupby("slide_id")["annot_label"].mean()
+        positivity_series = labels.groupby("slide_id")["label"].mean()
     return metadata["slide_id"].map(positivity_series).fillna(0.0).to_dict()
 
 
