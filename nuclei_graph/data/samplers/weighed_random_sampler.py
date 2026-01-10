e6e2f4308850204f3a1a9dff9ceffcb0e48c7e85
Karolína Rusnačková <525276@mail.muni.cz>
2026-01-10 10:11:21 +0000

    chore: rename var

--- nuclei_graph/data/samplers/weighed_random_sampler.py
+++ nuclei_graph/data/samplers/weighed_random_sampler.py
@@ -14,7 +14,7 @@ class AutoWeightedRandomSampler(WeightedRandomSampler):
     Arguments:
         dataset: Torch dataset class (NucleiDataset).
         slides_positivity: Dictionary mapping slide IDs to their positivity scores.
-        positivity_threshold: Threshold for considering a graph to be positive in the sampler.
+        positivity_thr: Threshold for considering a graph to be positive in the sampler.
         replacement: If True, samples are drawn with replacement. Default is True.
     """
 
@@ -22,22 +22,22 @@ class AutoWeightedRandomSampler(WeightedRandomSampler):
         self,
         dataset: NucleiDataset,
         slides_positivity: dict[str, float],
-        positivity_threshold: float = 0.0,
+        positivity_thr: float = 0.0,
         replacement: bool = True,
     ):
-        weights = self._get_weights(dataset, slides_positivity, positivity_threshold)
+        weights = self._get_weights(dataset, slides_positivity, positivity_thr)
         super().__init__(weights, num_samples=len(dataset), replacement=replacement)
 
     def _get_weights(
         self,
         dataset: NucleiDataset,
         slides_positivity: dict[str, float],
-        positivity_threshold: float,
+        positivity_thr: float,
     ) -> Sequence[float]:
         slide_ids = dataset.df_metadata["slide_id"].values
         labels = torch.tensor(
             [
-                1.0 if slides_positivity[slide_id] > positivity_threshold else 0.0
+                1.0 if slides_positivity[slide_id] > positivity_thr else 0.0
                 for slide_id in slide_ids
             ]
         )
