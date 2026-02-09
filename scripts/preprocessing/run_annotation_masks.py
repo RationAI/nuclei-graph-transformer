88132c843eba51eb7aa63992920c2556319f7541
Karolína Rusnačková <525276@mail.muni.cz>
2026-02-09 16:36:34 +0000

    feat: mem hints

--- scripts/preprocessing/run_annotation_masks.py
+++ scripts/preprocessing/run_annotation_masks.py
@@ -5,8 +5,8 @@ submit_job(
     job_name="nuclei-graph-annotation-masks",
     username=...,
     image="cerit.io/rationai/base:2.0.6",
-    cpu=4,
-    memory="64Gi",
+    cpu=6,
+    memory="4Gi",
     public=False,
     script=[
         "git clone git@gitlab.ics.muni.cz:rationai/digital-pathology/pathology/nuclei-graph-transformer.git workdir",
