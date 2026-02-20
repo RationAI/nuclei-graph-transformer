681f7a5e3f19897f921bbbc44fd5668734addfba
Karolína Rusnačková <525276@mail.muni.cz>
2026-02-20 16:08:34 +0000

    feat: tissue mask erosion

---
+++ scripts/preprocessing/run_tissue_masks.py
@@ -0,0 +1,21 @@
+from kube_jobs import storage, submit_job
+
+
+EXPERIMENT_NAME = ...  # "raw" or "edge_eroded"
+DATASET_NAME = "prostate_cancer_amacr"
+
+submit_job(
+    job_name="nuclei-graph-tissue-masks",
+    username=...,
+    image="cerit.io/rationai/base:2.0.6",
+    cpu=8,  # TODO
+    memory="64Gi",  # TODO
+    public=False,
+    script=[
+        "git clone git@gitlab.ics.muni.cz:rationai/digital-pathology/pathology/nuclei-graph-transformer.git workdir",
+        "cd workdir",
+        "uv sync --frozen",
+        f"uv run python -m preprocessing.tissue_masks +experiment=preprocessing/tissue_masks/{EXPERIMENT_NAME}/{DATASET_NAME}",
+    ],
+    storage=[storage.secure.DATA],
+)
