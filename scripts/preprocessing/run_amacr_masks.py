8c2c82fc9b8e72c7a61fdd985d4e65d151dff39e
Karolína Rusnačková <525276@mail.muni.cz>
2026-02-20 19:30:07 +0000

    feat: amacr mask refactor

---
+++ scripts/preprocessing/run_amacr_masks.py
@@ -0,0 +1,20 @@
+from kube_jobs import storage, submit_job
+
+
+DATASET_NAME = "prostate_cancer_amacr"
+
+submit_job(
+    job_name="nuclei-graph-amacr-masks",
+    username=...,
+    image="cerit.io/rationai/base:2.0.6",
+    cpu=8,  # TODO
+    memory="64Gi",  # TODO
+    public=False,
+    script=[
+        "git clone git@gitlab.ics.muni.cz:rationai/digital-pathology/pathology/nuclei-graph-transformer.git workdir",
+        "cd workdir",
+        "uv sync --frozen",
+        f"uv run python -m preprocessing.amacr_masks +experiment=preprocessing/amacr_masks/{DATASET_NAME}",
+    ],
+    storage=[storage.secure.DATA, storage.secure.PROJECTS],
+)
