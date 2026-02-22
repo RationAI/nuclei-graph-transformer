f6c8cb1ecbaa64a978f9e4530435ce64506b6e85
Karolína Rusnačková <525276@mail.muni.cz>
2026-02-22 18:41:06 +0000

    feat: run visualization script

---
+++ scripts/visualization/run_polygons_rasterization.py
@@ -0,0 +1,23 @@
+from kube_jobs import storage, submit_job
+
+
+EXPERIMENT_NAME = (
+    ...
+)  # one of "annotation_labels", "cam_labels", "outline_polygons", "predictions"
+DATASET_NAME = "prostate_cancer"
+
+submit_job(
+    job_name="nuclei-graph-polygons-rasterization",
+    username=...,
+    image="cerit.io/rationai/base:2.0.6",
+    cpu=2,
+    memory="190Gi",
+    public=False,
+    script=[
+        "git clone https://gitlab.ics.muni.cz/rationai/digital-pathology/pathology/nuclei-graph-transformer.git workdir",
+        "cd workdir",
+        "uv sync --frozen",
+        f"uv run -m visualization.polygons2raster +experiment=visualization/{EXPERIMENT_NAME} +data=datasets/raw/{DATASET_NAME}",
+    ],
+    storage=[storage.secure.DATA, storage.secure.PROJECTS],
+)
