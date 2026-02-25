1f17cb6fc98b2ebbb046d54c59a35353e867cb72
Karolína Rusnačková <525276@mail.muni.cz>
2026-02-25 09:37:39 +0000

    feat: delete slides callback

--- nuclei_graph/callbacks/prediction_metrics.py
+++ nuclei_graph/callbacks/prediction_metrics.py
@@ -48,37 +48,3 @@ class PredictionMetricsCallback(Callback):
         preds_sup = torch.sigmoid(logits_sup)
         metrics = cast("MetricCollection", pl_module.predict_metrics_global)
         metrics.update(preds_sup, targets_sup.long())
-
-
-class PredictionSlideMetricsCallback(Callback):
-    """Computes metrics per slide (prediction batch size is assumed to be 1)."""
-
-    def on_predict_batch_end(
-        self,
-        trainer: Trainer,
-        pl_module: LightningModule,
-        outputs: Tensor,
-        batch: PredictBatch,
-        batch_idx: int,
-        dataloader_idx: int = 0,
-    ) -> None:
-        slide = batch["slides"]  # batch size is 1
-        metadata = batch["metadata"][0]
-
-        targets_sup = slide["y"]
-        logits_sup = outputs.squeeze(-1)[slide["sup_mask"]]
-        assert targets_sup.shape == logits_sup.shape
-
-        metrics = cast("MetricCollection", pl_module.predict_metrics_slide)
-        metrics.update(torch.sigmoid(logits_sup), targets_sup.long())
-        computed_metrics = metrics.compute()
-
-        for key, value in computed_metrics.items():
-            metric_name = key.split("/")[-1]
-            mlflow.log_metric(
-                f"prediction/{metadata['slide_id']}/{metric_name}",
-                float(value),
-                step=pl_module.global_step,
-            )
-
-        metrics.reset()
