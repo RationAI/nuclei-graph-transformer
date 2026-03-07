87e4be135f26751774f77c111e7e2cf5bbc7b9f5
Karolína Rusnačková <525276@mail.muni.cz>
2026-03-07 10:47:19 +0000

    feat: nuclei heatmap labeling

--- scripts/preprocessing/run_unipolar_heatmap_labels.py
+++ scripts/preprocessing/run_unipolar_heatmap_labels.py
@@ -2,7 +2,7 @@ from kube_jobs import storage, submit_job
 
 
 submit_job(
-    job_name="nuclei-graph-annotation-nuclei-labeling",
+    job_name="nuclei-graph-heatmap-nuclei-labeling",
     username=...,
     image="cerit.io/rationai/base:2.0.6",
     cpu=8,
@@ -12,7 +12,7 @@ submit_job(
         "git clone https://gitlab.ics.muni.cz/rationai/digital-pathology/pathology/nuclei-graph-transformer.git workdir",
         "cd workdir",
         "uv sync --frozen",
-        "uv run -m preprocessing.annotation_labels +data=sources/...",
+        "uv run -m preprocessing.unipolar_heatmap_labels +experiment=preprocessing/unipolar_heatmap_labels/...",
     ],
     storage=[storage.secure.DATA, storage.secure.PROJECTS],
 )
