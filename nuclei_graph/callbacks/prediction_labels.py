c91e5922fc20f93f08cbc55baf3a29dd4d8c97c7
Karolína Rusnačková <525276@mail.muni.cz>
2026-03-14 20:44:18 +0000

    feat: pred callbacks

--- nuclei_graph/callbacks/prediction_labels.py
+++ nuclei_graph/callbacks/prediction_labels.py
@@ -8,7 +8,7 @@ from lightning import Callback, LightningModule, Trainer
 from nuclei_graph.nuclei_graph_typing import Outputs, PredictBatch
 
 
-class PredictionsCallback(Callback):
+class WSLPredictionsCallback(Callback):
     def __init__(
         self,
         mlflow_artifact_path: str = "predictions",
@@ -36,6 +36,58 @@ class PredictionsCallback(Callback):
 
         df = pd.DataFrame(
             {"id": metadata["nuclei_ids"], "prediction": predicted_labels}
+        )
+
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            output_path = f"{tmp_dir}/{metadata['slide_id']}.parquet"
+            df.to_parquet(output_path, index=False)
+
+            active_run = mlflow.active_run()
+            assert active_run is not None
+            mlflow.log_artifacts(
+                tmp_dir,
+                artifact_path=self.mlflow_artifact_path,
+                run_id=active_run.info.run_id,
+            )
+
+
+class MILPredictionsCallback(Callback):
+    def __init__(
+        self,
+        mlflow_artifact_path: str = "predictions",
+    ) -> None:
+        super().__init__()
+        self.mlflow_artifact_path = mlflow_artifact_path
+
+    def on_predict_batch_end(
+        self,
+        trainer: Trainer,
+        pl_module: LightningModule,
+        outputs: Outputs,
+        batch: PredictBatch,
+        batch_idx: int,
+        dataloader_idx: int = 0,
+    ) -> None:
+        metadata = batch["metadata"][0]  # batch size is 1
+        seq_len = batch["slides"]["seq_len"][0].item()
+
+        logits = outputs["nuclei"][0].squeeze(-1)  # (n,)
+        logits_unpadded = logits[:seq_len]
+        logits_ordered = logits_unpadded[metadata["perm_inverse"]]
+        nuclei_predicted_labels = torch.sigmoid(logits_ordered).cpu().numpy().flatten()
+
+        attn = outputs["attn_weights"][0].squeeze(-1)  # (n,)
+        attn_unpadded = attn[:seq_len]
+        attn_ordered = attn_unpadded[metadata["perm_inverse"]]
+        attn_scores = attn_ordered.cpu().numpy().flatten()
+
+        df = pd.DataFrame(
+            {
+                "id": metadata["nuclei_ids"],
+                "nuclei_prediction": nuclei_predicted_labels,
+                "attention_score": attn_scores,
+                "graph_prediction": torch.sigmoid(outputs["graph"][0]).item(),
+            }
         )
 
         with tempfile.TemporaryDirectory() as tmp_dir:
--- nuclei_graph/callbacks/prediction_metrics.py
+++ nuclei_graph/callbacks/prediction_metrics.py
@@ -8,8 +8,8 @@ from torchmetrics import MetricCollection
 from nuclei_graph.nuclei_graph_typing import Outputs, PredictBatch
 
 
-class PredictionMetricsCallback(Callback):
-    """Computes global metrics across the entire dataset."""
+class WSLPredictionMetricsCallback(Callback):
+    """Computes nuclei-level global metrics across the entire dataset."""
 
     def on_predict_epoch_end(
         self,
@@ -47,3 +47,61 @@ class PredictionMetricsCallback(Callback):
         preds_sup = torch.sigmoid(logits_sup)
         metrics = cast("MetricCollection", pl_module.predict_metrics)
         metrics.update(preds_sup, targets_sup.long())
+
+
+class MILPredictionMetricsCallback(Callback):
+    """Computes slide-level and nucleus-level metrics across the entire dataset."""
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def on_predict_batch_end(
+        self,
+        trainer: Trainer,
+        pl_module: LightningModule,
+        outputs: Outputs,
+        batch: PredictBatch,
+        batch_idx: int,
+        dataloader_idx: int = 0,
+    ) -> None:
+        slide = batch["slides"]  # batch size is 1
+
+        targets_graph = slide["y"]["graph"]
+        if targets_graph is not None:
+            logits_graph = outputs["graph"].view(-1)
+            targets_graph = targets_graph.view(-1)
+
+            preds_graph = torch.sigmoid(logits_graph)
+            graph_metrics = cast("MetricCollection", pl_module.predict_graph_metrics)
+            graph_metrics.update(preds_graph, targets_graph.long())
+
+        targets_sup = slide["y"]["nuclei"]
+        if targets_sup is not None and targets_sup.numel() > 0:
+            logits_sup = outputs["nuclei"][slide["sup_mask"]].squeeze(-1)
+            assert targets_sup.shape == logits_sup.shape
+
+            preds_sup = torch.sigmoid(logits_sup)
+            nuclei_metrics = cast("MetricCollection", pl_module.predict_nuclei_metrics)
+            nuclei_metrics.update(preds_sup, targets_sup.long())
+
+    def on_predict_epoch_end(
+        self,
+        trainer: Trainer,
+        pl_module: LightningModule,
+    ) -> None:
+
+        graph_metrics = cast("MetricCollection", pl_module.predict_graph_metrics)
+        computed_graph = graph_metrics.compute()
+        for key, value in computed_graph.items():
+            metric_name = key.split("/")[-1]
+            mlflow.log_metric(f"prediction/graph/{metric_name}", float(value))
+
+        graph_metrics.reset()
+
+        nuclei_metrics = cast("MetricCollection", pl_module.predict_nuclei_metrics)
+        computed_nuclei = nuclei_metrics.compute()
+        for key, value in computed_nuclei.items():
+            metric_name = key.split("/")[-1]
+            mlflow.log_metric(f"prediction/nuclei/{metric_name}", float(value))
+
+        nuclei_metrics.reset()
