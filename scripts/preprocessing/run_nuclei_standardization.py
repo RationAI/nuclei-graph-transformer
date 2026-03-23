a014e05f732dfa42b71c333770fa94d7717a150c
Karolína Rusnačková <525276@mail.muni.cz>
2026-03-23 15:45:58 +0000

    feat: rename file

--- scripts/preprocessing/run_nuclei_standardization.py
+++ scripts/preprocessing/run_nuclei_standardization.py
@@ -12,7 +12,7 @@ submit_job(
         "git clone https://github.com/RationAI/nuclei-graph-transformer.git workdir",
         "cd workdir",
         "uv sync --frozen",
-        "uv run -m preprocessing.standardize_nuclei +data=sources/panda",
+        "uv run -m preprocessing.nuclei_standardization +data=sources/panda",
     ],
     storage=[storage.secure.DATA, storage.secure.PROJECTS],
 )
