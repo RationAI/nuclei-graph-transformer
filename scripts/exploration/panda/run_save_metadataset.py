d601c0de8c1f716f367e4567ee0c672a27c5fa84
Karolína Rusnačková <525276@mail.muni.cz>
2026-03-23 18:47:25 +0000

    feat: job scripts

---
+++ scripts/exploration/panda/run_save_metadataset.py
@@ -0,0 +1,18 @@
+from kube_jobs import storage, submit_job
+
+
+submit_job(
+    job_name="nuclei-graph-data-exploration-panda",
+    username=...,
+    image="cerit.io/rationai/base:2.0.6",
+    cpu=20,
+    memory="64Gi",
+    public=False,
+    script=[
+        "git clone https://github.com/RationAI/nuclei-graph-transformer.git workdir",
+        "cd workdir",
+        "uv sync --frozen",
+        "uv run -m exploration.panda.save_metadataset +data=sources/panda",
+    ],
+    storage=[storage.secure.DATA],
+)
--- scripts/exploration/prostate_cancer/run_save_metadataset.py
+++ scripts/exploration/prostate_cancer/run_save_metadataset.py
@@ -2,17 +2,17 @@ from kube_jobs import storage, submit_job
 
 
 submit_job(
-    job_name="nuclei-graph-data-exploration",
+    job_name="nuclei-graph-data-exploration-prostate_cancer",
     username=...,
     image="cerit.io/rationai/base:2.0.6",
-    cpu=20,
-    memory="64Gi",
+    cpu=1,
+    memory="4Gi",
     public=False,
     script=[
         "git clone https://github.com/RationAI/nuclei-graph-transformer.git workdir",
         "cd workdir",
         "uv sync --frozen",
-        "uv run -m exploration.<DATASET_NAME>.save_metadataset +data=sources/...",
+        "uv run -m exploration.prostate_cancer.save_metadataset +data=sources/prostate_cancer",
     ],
     storage=[storage.secure.DATA],
 )
