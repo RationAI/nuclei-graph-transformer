e9837ffa8e89f08dc7bd94dac0259d55dc8f7a3e
Karolína Rusnačková <525276@mail.muni.cz>
2026-03-27 23:11:56 +0000

    fix: bugs

--- preprocessing/metadata_mapping/panda.py
+++ preprocessing/metadata_mapping/panda.py
@@ -23,18 +23,11 @@ from rationai.mlkit.lightning.loggers import MLFlowLogger
 @autolog
 def main(config: DictConfig, logger: MLFlowLogger) -> None:
     slides = pd.read_csv(Path(download_artifacts(config.metadata_uri)))
-    valid_slides = slides[
-        slides["has_annotation"]
-        & slides["has_segmentation"]
-        & ~slides["is_annotation_corrupted"]
-        & slides["is_wsi_valid"]
-    ]
-    
     split = pd.read_csv(Path(download_artifacts(config.split_uri)))
-    valid_slides = slides.merge(split, on="slide_id", how="inner")
+    slides = slides.merge(split, on="slide_id", how="inner")
 
     nuclei_dir = Path(config.nuclei_path)
-    nuclei_paths = valid_slides["slide_id"].map(
+    nuclei_paths = slides["slide_id"].map(
         lambda id: nuclei_dir / f"slide_id={id}"
     )
     nuclei_counts = nuclei_paths.map(str).apply(
@@ -43,14 +36,14 @@ def main(config: DictConfig, logger: MLFlowLogger) -> None:
 
     map_df = pd.DataFrame(
         {
-            "slide_id": valid_slides["slide_id"],
-            "slide_path": valid_slides["slide_path"],
+            "slide_id": slides["slide_id"],
+            "slide_path": slides["slide_path"],
             "slide_nuclei_path": nuclei_paths.map(str),
             "nuclei_count": nuclei_counts.astype("Int64"),
-            "is_carcinoma": valid_slides["isup_grade"] >= 1,
-            "mpp_x": valid_slides["mpp_x"],
-            "mpp_y": valid_slides["mpp_y"],
-            "set": valid_slides["set"],
+            "is_carcinoma": slides["isup_grade"] >= 1,
+            "mpp_x": slides["mpp_x"],
+            "mpp_y": slides["mpp_y"],
+            "set": slides["set"],
         }
     )
     
