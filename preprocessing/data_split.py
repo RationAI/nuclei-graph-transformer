a6fb80ef2a37b94940becd37314f731c9c7fe878
Karolína Rusnačková <525276@mail.muni.cz>
2026-03-27 21:45:02 +0000

    feat: train-test split

---
+++ preprocessing/data_split.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+import tempfile
+
+import hydra
+import pandas as pd
+from omegaconf import DictConfig
+from rationai.mlkit import autolog, with_cli_args
+from rationai.mlkit.lightning.loggers import MLFlowLogger
+from sklearn.model_selection import train_test_split
+from mlflow.artifacts import download_artifacts
+
+
+@with_cli_args(["+preprocessing=data_split"])
+@hydra.main(config_path="../configs", config_name="preprocessing", version_base=None)
+@autolog
+def main(config: DictConfig, logger: MLFlowLogger) -> None:
+    slides = pd.read_csv(Path(download_artifacts(config.metadata_uri)))
+    
+    if config.restriction is not None:
+        slides = slides[slides[config.restriction.subset_column] == config.restriction.target_value]
+        
+    train, test = train_test_split(
+        slides,
+        test_size=config.split_size,
+        random_state=42,
+        stratify=slides[config.stratify_column],
+    )
+    train['set'] = 'train'
+    test['set'] = 'test'
+    split = pd.concat([train, test], ignore_index=True)
+    
+    summary = (
+        split.groupby(["set", config.stratify_column])
+        .size()
+        .reset_index(name="count")
+    )
+    summary[f"{config.stratify_column}_ratio"] = summary.groupby("set")["count"].transform(
+    lambda x: round(x / x.sum(), 2)
+)
+
+    with tempfile.TemporaryDirectory() as output_dir:
+        split[["slide_id", "set"]].to_csv(Path(output_dir) / "split.csv", index=False)
+        logger.log_artifact(str(Path(output_dir) / "split.csv"))
+        summary.to_csv(Path(output_dir) / "summary.csv", index=False)
+        logger.log_artifact(str(Path(output_dir) / "summary.csv"))
+
+if __name__ == "__main__":
+    main()
