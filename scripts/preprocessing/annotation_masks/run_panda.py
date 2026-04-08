002d3e51d3bff2c20878e22f7bca3e9327407438
karolina <k.rusnackova@gmail.com>
2026-04-08 14:21:16 +0000

    fix: job script

--- scripts/preprocessing/annotation_masks/run_panda.py
+++ scripts/preprocessing/annotation_masks/run_panda.py
@@ -9,8 +9,6 @@ submit_job(
     script=[
         "git clone https://github.com/RationAI/nuclei-graph-transformer.git workdir",
         "cd workdir",
-        "git checkout panda/annotation-masks",
-        "git checkout a4778033fc80e620e52578e47666c05ff50447cd",
         "export MLFLOW_TRACKING_USERNAME=...",
         "export MLFLOW_TRACKING_PASSWORD=...",
         "export MLFLOW_TRACKING_URI='https://mlflow.rationai.cloud.e-infra.cz/'",
