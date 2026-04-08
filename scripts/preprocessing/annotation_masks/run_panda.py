dbf3cd1a2f9015d2b8d1bb448efc2a70e1da9802
karolina <k.rusnackova@gmail.com>
2026-04-08 14:20:40 +0000

    fix: job script

--- scripts/preprocessing/annotation_masks/run_panda.py
+++ scripts/preprocessing/annotation_masks/run_panda.py
@@ -2,17 +2,21 @@ from kube_jobs import storage, submit_job
 
 
 submit_job(
-    job_name="nuclei-graph-annotation-masks-panda",
+    job_name="nuclei-graph-annot-masks-panda",
     username=...,
-    image="cerit.io/rationai/base:2.0.6",
-    cpu=8,
+    cpu=16,
     memory="64Gi",
-    public=False,
     script=[
         "git clone https://github.com/RationAI/nuclei-graph-transformer.git workdir",
         "cd workdir",
-        "uv sync --frozen",
-        "uv run -m preprocessing.annotation_masks.panda",
+        "git checkout panda/annotation-masks",
+        "git checkout a4778033fc80e620e52578e47666c05ff50447cd",
+        "export MLFLOW_TRACKING_USERNAME=...",
+        "export MLFLOW_TRACKING_PASSWORD=...",
+        "export MLFLOW_TRACKING_URI='https://mlflow.rationai.cloud.e-infra.cz/'",
+        "uv sync",
+        "uv run python -m preprocessing.annotation_masks.panda",
     ],
-    storage=[storage.secure.DATA],
+    public=True,
+    storage=[storage.public.DATA],
 )
