5a7d1626fb54b2b59ac4c1a68e6d461066fcf957
Karolína Rusnačková <129358914+xrusnack@users.noreply.github.com>
2026-04-09 12:38:24 +0200

    feat(panda): annotation-based nuclei labeling (#10)

--- scripts/visualization/run_polygons_rasterization.py
+++ scripts/visualization/run_polygons_rasterization.py
@@ -2,17 +2,17 @@ from kube_jobs import storage, submit_job
 
 
 submit_job(
-    job_name="nuclei-graph-data-exploration-prostate_cancer_mmci_tl",
+    job_name="nuclei-graph-polygons-rasterization",
     username=...,
     image="cerit.io/rationai/base:2.0.6",
-    cpu=1,
-    memory="4Gi",
+    cpu=2,
+    memory="190Gi",
     public=False,
     script=[
         "git clone https://github.com/RationAI/nuclei-graph-transformer.git workdir",
         "cd workdir",
         "uv sync --frozen",
-        "uv run -m exploration.prostate_cancer_mmci_tl.save_metadataset +data=sources/prostate_cancer_mmci_tl",
+        "uv run -m visualization.polygons2raster +visualization=polygons2raster/... +experiment=visualization/...",
     ],
-    storage=[storage.secure.DATA],
+    storage=[storage.secure.DATA, storage.secure.PROJECTS],
 )
