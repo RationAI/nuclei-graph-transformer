1585970c761b5ccab1a318bdff3798e9b30bf298
Karolína Rusnačková <525276@mail.muni.cz>
2026-04-14 22:04:02 +0000

    chore: update jobs for public cloud

--- scripts/preprocessing/metadata_mapping/run_prostate_cancer_mmci_tl.py
+++ scripts/preprocessing/metadata_mapping/run_prostate_cancer_mmci_tl.py
@@ -2,17 +2,20 @@ from kube_jobs import storage, submit_job
 
 
 submit_job(
-    job_name="nuclei-graph-metadata-mapping-prostate_cancer-mmci-tl",
+    job_name="nuclei-graph-metadata-mapping-panda",
     username=...,
     image="cerit.io/rationai/base:2.0.6",
     cpu=1,
     memory="4Gi",
-    public=False,
+    public=True,
     script=[
         "git clone https://github.com/RationAI/nuclei-graph-transformer.git workdir",
         "cd workdir",
+        "export MLFLOW_TRACKING_USERNAME='...'",
+        "export MLFLOW_TRACKING_PASSWORD='...'",
+        "export MLFLOW_TRACKING_URI='https://mlflow.rationai.cloud.e-infra.cz/'",
         "uv sync --frozen",
-        "uv run -m preprocessing.metadata_mapping.prostate_cancer_mmci_tl",
+        "uv run python -m preprocessing.metadata_mapping.panda +experiment=preprocessing/metadata_mapping/...",
     ],
-    storage=[storage.secure.DATA, storage.secure.PROJECTS],
+    storage=[storage.public.DATA, storage.public.PROJECTS],
 )
