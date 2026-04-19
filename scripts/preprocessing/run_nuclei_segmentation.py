c1a168f42afa21ece068f7f04b25b31f85203db3
Karolína Rusnačková <525276@mail.muni.cz>
2026-04-19 18:08:25 +0000

    feat: job-scripts PR changes

--- scripts/preprocessing/run_nuclei_segmentation.py
+++ scripts/preprocessing/run_nuclei_segmentation.py
@@ -10,7 +10,7 @@ submit_job(
     gpu="H100",
     public=False,
     script=[
-        "git clone https://gitlab.ics.muni.cz/rationai/digital-pathology/pathology/nuclei-graph-transformer.git workdir",
+        "git clone https://github.com/RationAI/nuclei-graph-transformer.git workdir",
         "cd workdir",
         "uv sync --frozen",
         "uv run -m preprocessing.nuclei_segmentation +data=sources/...",
