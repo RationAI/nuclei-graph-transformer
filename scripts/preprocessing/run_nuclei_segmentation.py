136fe74c77eb4ff04f46ae83f2ab668ebee33f6e
Karolína Rusnačková <129358914+xrusnack@users.noreply.github.com>
2026-04-28 19:46:26 +0200

    feat: job scripts (#14)

--- scripts/preprocessing/run_nuclei_segmentation.py
+++ scripts/preprocessing/run_nuclei_segmentation.py
@@ -2,17 +2,18 @@ from kube_jobs import storage, submit_job
 
 
 submit_job(
-    job_name="nuclei-graph-annotation-nuclei-labeling",
+    job_name="nuclei-graph-nuclei-segmentation",
     username=...,
     image="cerit.io/rationai/base:2.0.6",
-    cpu=8,
-    memory="64Gi",
+    cpu=20,
+    memory="80Gi",
+    gpu="H100",
     public=False,
     script=[
         "git clone https://github.com/RationAI/nuclei-graph-transformer.git workdir",
         "cd workdir",
         "uv sync --frozen",
-        "uv run -m preprocessing.annotation_labels +experiment=preprocessing/annotation_labels/...",
+        "uv run -m preprocessing.nuclei_segmentation +data=sources/...",
     ],
     storage=[storage.secure.DATA, storage.secure.PROJECTS],
 )
