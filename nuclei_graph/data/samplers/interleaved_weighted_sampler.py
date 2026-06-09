e80a385f1bd1e9bb46133474508121a128063d43
Karolína Rusnačková <525276@mail.muni.cz>
2026-06-09 08:41:57 +0000

    feat: config setup

--- nuclei_graph/data/samplers/interleaved_weighted_sampler.py
+++ nuclei_graph/data/samplers/interleaved_weighted_sampler.py
@@ -10,47 +10,68 @@ class StratifiedInterleavedSlideSampler(Sampler):
         tiles_df: pd.DataFrame,
         target_col: str = "carcinoma",
         max_active_slides: int = 3,
+        samples_per_epoch: int = 10000,
     ):
         self.tiles_df = tiles_df
         self.max_active_slides = max_active_slides
         self.target_col = target_col
+        self.samples_per_epoch = min(samples_per_epoch, len(tiles_df))
 
-        self.slide_groups = {
+        self.master_slide_groups = {
             k: list(v) for k, v in tiles_df.groupby("slide_id").indices.items()
         }
 
-        slide_labels = tiles_df.groupby("slide_id")[target_col].first()
+        self.slide_labels = tiles_df.groupby("slide_id")[target_col].first()
 
-        value_counts = slide_labels.value_counts()
-        slide_weights = 1.0 / slide_labels.map(value_counts)
+        self.remaining_tiles = {}
+        self.active_slides = []
 
-        self.slide_ids = slide_labels.index.tolist()
-        self.slide_weights = [float(w) for w in slide_weights.values]
+        self._reset_global_state()
 
-        for slide_id in self.slide_groups:
-            random.shuffle(self.slide_groups[slide_id])
+    def _reset_global_state(self):
+        self.remaining_tiles = {k: list(v) for k, v in self.master_slide_groups.items()}
+        for slide_id in self.remaining_tiles:
+            random.shuffle(self.remaining_tiles[slide_id])
+
+        self.active_slides = []
+
+    def _get_next_slide(self):
+        available_slides = [
+            s
+            for s, tiles in self.remaining_tiles.items()
+            if len(tiles) > 0 and s not in self.active_slides
+        ]
+
+        if not available_slides:
+            return None
+
+        labels_subset = self.slide_labels[available_slides]
+        value_counts = labels_subset.value_counts()
+        weights = 1.0 / labels_subset.map(value_counts)
+
+        return random.choices(available_slides, weights=weights.tolist(), k=1)[0]
 
     def __iter__(self):
-        epoch_groups = {k: list(v) for k, v in self.slide_groups.items()}
+        yielded_count = 0
 
-        chosen_slide_order = random.choices(
-            self.slide_ids, weights=self.slide_weights, k=len(self.slide_ids)
-        )
+        while yielded_count < self.samples_per_epoch:
+            while len(self.active_slides) < self.max_active_slides:
+                next_slide = self._get_next_slide()
+                if next_slide is not None:
+                    self.active_slides.append(next_slide)
+                else:
+                    break
 
-        active_slides = []
+            if not self.active_slides:
+                self._reset_global_state()
+                continue
 
-        while len(active_slides) < self.max_active_slides and chosen_slide_order:
-            active_slides.append(chosen_slide_order.pop(0))
+            current_slide = random.choice(self.active_slides)
+            yield self.remaining_tiles[current_slide].pop()
+            yielded_count += 1
 
-        while active_slides:
-            current_slide = random.choice(active_slides)
-
-            yield epoch_groups[current_slide].pop()
-
-            if not epoch_groups[current_slide]:
-                active_slides.remove(current_slide)
-                if chosen_slide_order:
-                    active_slides.append(chosen_slide_order.pop(0))
+            if not self.remaining_tiles[current_slide]:
+                self.active_slides.remove(current_slide)
 
     def __len__(self):
-        return len(self.tiles_df)
+        return self.samples_per_epoch
