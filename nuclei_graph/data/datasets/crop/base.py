d54dc47f0df806a8b5b78dea89b1dac42400672f
Karolína Rusnačková <525276@mail.muni.cz>
2026-06-30 16:30:58 +0000

    fix: typing

--- nuclei_graph/data/datasets/crop/base.py
+++ nuclei_graph/data/datasets/crop/base.py
@@ -2,7 +2,7 @@ import heapq
 import math
 from abc import ABC, abstractmethod
 from random import choice, randint, randrange, uniform
-from typing import NamedTuple, Type
+from typing import NamedTuple
 
 import numpy as np
 import pandas as pd
