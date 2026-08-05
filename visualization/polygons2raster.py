cb7e669841ae14b98741ee9a70351e114436b63c
karolina <k.rusnackova@gmail.com>
2026-08-05 19:03:02 +0000

    feat: negative labels

--- visualization/polygons2raster.py
+++ visualization/polygons2raster.py
@@ -58,12 +58,12 @@ def set_filling_and_get_outline_color(
             assert heatmap_labels_dir is not None and label_column is not None
             heatmap_path = heatmap_labels_dir / f"{slide_path.stem}.parquet"
             if not heatmap_path.exists():  # negative slide
-                # nuclei["fill_color"] = 255
+                nuclei["fill_color"] = 255
                 return nuclei, outline_color
             heatmap_df = pd.read_parquet(heatmap_path)
             nuclei = nuclei.merge(heatmap_df, on="id", how="inner")
-            nuclei.loc[nuclei[label_column] == 1, "fill_color"] = 255
-            # nuclei.loc[nuclei[label_column] == 0, "fill_color"] = 255
+            # nuclei.loc[nuclei[label_column] == 1, "fill_color"] = 255
+            nuclei.loc[nuclei[label_column] == 0, "fill_color"] = 255
 
         case 4:  # CAM-based Pseudo Labeling
             assert cam_labels_dir is not None
