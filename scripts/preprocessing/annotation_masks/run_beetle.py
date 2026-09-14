3aa0acf878e49fb05ceaad874c40db523794e1c7
Karolína Rusnačková <525276@mail.muni.cz>
2026-09-14 14:08:59 +0000

    feat: add beetle annotations

---
+++ scripts/preprocessing/annotation_masks/run_beetle.py
@@ -0,0 +1,21 @@
+from kube_jobs import storage, submit_job
+
+
+submit_job(
+    job_name="nuclei-graph-annotation-masks-beetle",
+    username=...,
+    image="cerit.io/rationai/base:2.0.6",
+    cpu=8,
+    memory="64Gi",
+    public=True,
+    script=[
+        "git clone https://github.com/RationAI/nuclei-graph-transformer.git workdir",
+        "cd workdir",
+        "export MLFLOW_TRACKING_USERNAME='...'",
+        "export MLFLOW_TRACKING_PASSWORD='...'",
+        "export MLFLOW_TRACKING_URI='https://mlflow.rationai.cloud.e-infra.cz/'",
+        "uv sync --frozen",
+        "uv run python -m preprocessing.annotation_masks.beetle",
+    ],
+    storage=[storage.public.DATA, storage.public.PROJECTS],
+)
